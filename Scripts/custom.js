3a53e59d70ea854334c4d8b129fc33b361c7f6f0
prbhasva <prbhavsa@my.bridgeport.edu>
2018-04-03 13:41:33 -0400

    Removed the Looping Speech

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -3,9 +3,3 @@ function GoHome(){
     PlaySpeech("Enjoy the your stay at San Antonio Airport");
     location.href = "maincontents.htm";
 }
-
-PlaySpeech("Please remove FINAL FOUR programs from checked bags and place inside your carry-on bag to prevent delays in processing baggage.");
-
-setInterval(function(){
-    PlaySpeech("Please remove FINAL FOUR programs from checked bags and place inside your carry-on bag to prevent delays in processing baggage.");
-}, 20000);
